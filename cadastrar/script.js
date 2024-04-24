e1d84d252de2ffcc5826d236f0a3900a4f46a8ce
leaelthiago <leaelthiago774@gmail.com>
2024-04-24 16:11:41 -0300

    Alteração cadastro 1.1

--- cadastrar/script.js
+++ cadastrar/script.js
@@ -1,6 +1,8 @@
 const nomeInput = document.getElementById('nome');
 const nascimentoInput = document.getElementById('nascimento');
+const cpfInput = document.getElementById('CPF');
 const emailInput = document.getElementById('email');
+const generoInput = document.getElemntById('genero');
 const senhaInput = document.getElementById('senha');
 const tabelaCadastroDiv = document.getElementById('tabelaCadastro');
 
@@ -16,14 +18,18 @@ function CadastrarUsuarios(){
     //Acessa o valor do input
     nomeInput.value;
     nascimentoInput.value;
+    cpfInput.value;
     emailInput.value;
+    generoInput.value;
     senhaInput.value;
 
         //Criando objeto de pessoa (JSON)
         const pessoa = {};
         pessoa.nome = nomeInput.value;
         pessoa.nascimento = nascimentoInput.value;
+        pessoa.CPF = cpfInput.value;
         pessoa.email = emailInput.value;
+        pessoa.genero = generoInput.value;
         pessoa.senha = senhaInput.value;
         console.log(pessoa);
 
@@ -35,6 +41,14 @@ function CadastrarUsuarios(){
 
             localStorage.setItem('pessoasCadastro', JSON.stringify(pessoasCadastradas));//JSON.stringify() - Pega o objeto e transforma em texto
             mostraPessoa(pessoa);
+
+                //Zerando os campos após cadastrar
+                nomeInput.value = "";
+                nascimentoInput.value = "";
+                cpfInput.value = "";
+                emailInput.value ="";
+                generoInput.value = "";
+                senhaInput.value = "";
         
         return false;
 }
@@ -47,8 +61,14 @@ function mostraPessoa(i){
     //Nascimento
     tabelaCadastroDiv.innerHTML += `<span style="color: white; background-color: black; border: 3px solid black; display: inline-block; padding: 0px 10px;"><strong>Data nascimento: </strong> ${i.nascimento}</span>`;
 
+    //CPF
+    tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: yellow; border: 3px solid black; display: inline-block; padding: 0px 10px;"><strong>CPF: </strong> ${i.CPF}</span>`;
+
     //Email
     tabelaCadastroDiv.innerHTML += `<span style="color: yellow; background-color: purple; border: 3px solid black; display: inline-block; padding: 0px 10px;"><strong>Email: </strong> ${i.email}</span>`;
+
+    //Genero
+    tabelaCadastroDiv.innerHTML += `<span style="color: blue; background-color: white; border: 3px solid black; display: inline-block; padding: 0px 10px; <strong>Genero: </strong> ${i.genero}<>">`
 
     //Senha
     tabelaCadastroDiv.innerHTML += `<span style="color: blue; background-color: gray; border: 3px solid black; display: inline-block; padding: 0px 10px;"><strong>Senha: </strong> ${i.senha}</span><br/>`;
