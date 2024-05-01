01c371ce76511d4cc4f8f4fca0626e9ad6772a64
Leael Thiago de Souza <160546949+LeaelSouza@users.noreply.github.com>
2024-04-30 22:49:28 -0300

    Finalização

--- consulta/consultaUsuario.js
+++ consulta/consultaUsuario.js
@@ -4,69 +4,45 @@ const cpfInput = document.getElementById('CPF');
 const emailInput = document.getElementById('email');
 const generoInput = document.getElementById('genero');
 const senhaInput = document.getElementById('senha');
+
 const tabelaCadastroDiv = document.getElementById('tabelaCadastro');
 
-let contador = 0;
-    const pessoas = pegaPessoasCadastradas();
-            for(const pessoa of pessoas) {
-                mostraPessoa(pessoa);
-                contador = contador++;
-            }
+let pessoas = pegaPessoasCadastradas();
+mostraPessoas();
 
-        // const pessoas = pegaPessoasCadastradas();
-        //     for (let index = 0; index < pessoas.length; index++) {
-        //         const pessoa = pessoas[index];
-        //         mostraPessoa(pessoa, index); // Passando o índice para a função mostraPessoa
-        // }
+function mostraPessoas() {
+    tabelaCadastroDiv.innerHTML = '';
+    pessoas.forEach((pessoa) => {
+        mostraPessoa(pessoa);
+    });
+}
 
+function mostraPessoa(pessoa, index) {
+    tabelaCadastroDiv.innerHTML += `
+        <br/>
+        <div class="linha ${index % 2 === 0 ? 'par' : 'impar'}">
+            <span><strong>Nome: </strong> <span id="nome_${pessoa.Nome}">${pessoa.Nome}</span></span>
+            <span><strong>Nascimento: </strong> <span id="nascimento_${pessoa.Nome}">${pessoa.Nascimento}</span></span>
+            <span><strong>CPF: </strong> <span id="cpf_${pessoa.Nome}">${pessoa.cpf}</span></span>
+            <span><strong>Email: </strong> <span id="email_${pessoa.Nome}">${pessoa.Email}</span></span>
+            <span><strong>Gênero: </strong> <span id="genero_${pessoa.Nome}">${pessoa.Genero}</span></span>
+            <span><strong>Senha: </strong> <span id="senha_${pessoa.Nome}">${pessoa.Senha}</span></span>
+        </div>
+        <button onclick="window.deletarUsuario('${pessoa.Nome}')">Deletar</button>
+        <br/><br/>`;
+}
 
+window.deletarUsuario = function(nome) {
+    pessoas = pessoas.filter(pessoa => pessoa.Nome !== nome);
+    atualizarLocalStorage();
+    mostraPessoas();
+};
 
-    //             function mostraPessoa(i){
+function pegaPessoasCadastradas() {
+    let pessoasCadastradas = JSON.parse(localStorage.getItem("cadastroPessoas"));
+    return pessoasCadastradas || [];
+}
 
-    //                 tabelaCadastroDiv.innerHTML +=`<br/><span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Nome: </strong> ${i.Nome}</span>`;
-    //                 tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Data nascimento: </strong> ${i.Nascimento}</span>`;
-    //                 tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>CPF: </strong> ${i.cpf}</span>`;
-    //                 tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Email: </strong> ${i.Email}</span>`;
-    //                 tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Genero: </strong> ${i.Genero}</span>`;
-    //                 tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Senha: </strong> ${i.Senha}</span><br/>`;
-
-    //                     tabelaCadastroDiv.innerHTML +=`<p></p>`
-    //             }
-
-
-                function mostraPessoa(i, contador){
-
-                    let resultado = pegaPessoasCadastradas(contador);
-                    console.log(resultado);
-
-                    let rowCase = resultado % 2 == 0 ? 'par' : 'impar';
-
-                    tabelaCadastroDiv.innerHTML +=`<br/>
-                        <div class="linha ${rowCase}">
-                            <span><strong>Nome: </strong> ${i.Nome}</span>
-                            <span><strong>Nascimento: </strong> ${i.Nascimento}</span>
-                            <span><strong>CPF: </strong> ${i.cpf}</span>
-                            <span><strong>Email: </strong> ${i.Email}</span>
-                            <span><strong>Gênero: </strong> ${i.Genero}</span>
-                            <span><strong>Senha: </strong> ${i.Senha}</span>
-                            
-                        </div><br/>`;
-
-                    // tabelaCadastroDiv.innerHTML +=`<br/><span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Nome: </strong> ${i.Nome}</span>`;
-                    // tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Data nascimento: </strong> ${i.Nascimento}</span>`;
-                    // tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>CPF: </strong> ${i.cpf}</span>`;
-                    // tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Email: </strong> ${i.Email}</span>`;
-                    // tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Genero: </strong> ${i.Genero}</span>`;
-                    // tabelaCadastroDiv.innerHTML += `<span style="color: black; background-color: white; border: 1px dashed black; display: inline-block; padding: 0px 10px;"><strong>Senha: </strong> ${i.Senha}</span><br/>`;
-
-                    //     tabelaCadastroDiv.innerHTML +=`<p></p>`
-                }
-                    
-                    function pegaPessoasCadastradas(){
-                        let pessoasCadastradas = JSON.parse(localStorage.getItem("cadastroPessoas"));
-                            if(pessoasCadastradas === null){
-                                pessoasCadastradas = [];
-                            }
-                        
-                            return pessoasCadastradas;
-                    }
+function atualizarLocalStorage() {
+    localStorage.setItem("cadastroPessoas", JSON.stringify(pessoas));
+}
